fbed1c5d2f4060d890e84073109bcb5f82c5ba9e
SHARMA Shubham <shubham@sharma.fr>
2015-10-23 23:45:30 +0200

    frame ok

--- server.js
+++ server.js
@@ -11,10 +11,32 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
+// this is where the Parse API should make a post reques
 app.post('/parse/', function (req, res) {
 	res.send(req.body);
 	console.log(req.body);
 });
+
+// This function handle the attachment
+function handleAttachment(att){
+	//I am handling the attachment which is in PNG
+
+	// I need to resize the photo in order to cut unused space
+
+	// Revert the transparent with white color and the non-transparent in transparent
+}
+
+function handleHashtag(hash){
+
+}
+
+function sendEmail(address){
+
+}
+
+function save(finalImage, date, adress, hash, picFrame){
+
+}
 
 
 var server = app.listen(3000, function () {
