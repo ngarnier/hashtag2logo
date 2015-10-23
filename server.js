c391c31588482c0723b776f5783b0f4843955742
SHARMA Shubham <shubham@sharma.fr>
2015-10-23 23:36:49 +0200

    server post request parse ok

--- server.js
+++ server.js
@@ -1,1 +1,25 @@
 // This is server
+
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.post('/parse/', function (req, res) {
+	res.send(req.body);
+	console.log(req.body);
+});
+
+
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
