4ab76a09d191a9f1a2eaff2f537a357a41ff8a13
SHARMA Shubham <shubham@sharma.fr>
2015-10-24 02:42:50 +0200

    commit

--- image_test2.js
+++ image_test2.js
@@ -1,32 +1,29 @@
 var fs = require('fs'),
     PNG = require('pngjs').PNG;
 
-fs.createReadStream('images/ps.png')
+var base = [];
 
+fs.createReadStream('images/google.png')
 .pipe(new PNG({
     filterType: 4
 }))
-
 .on('parsed', function() {
 
     for (var y = 0; y < this.height; y++) {
         for (var x = 0; x < this.width; x++) {
             var idx = (this.width * y + x) << 2;
 
-            if (this.data[idx] == 0 && this.data[idx + 1] == 0 && this.data[idx + 2] == 0){
-                this.data[idx] = 255;
-                this.data[idx+1] = 255;
-                this.data[idx+2] = 255;
-                this.data[idx+3] = 255;
-            }
-            else{
-                this.data[idx] = 255;
-                this.data[idx+1] = 255;
-                this.data[idx+2] = 255;
-                this.data[idx+3] = 0;
-            }
+            base[idx] = this.data[idx];
+            base[idx] = this.data[idx+1];
+            base[idx] = this.data[idx+2];
+            base[idx] = this.data[idx+3];
+
         }
     }
 
     this.pack().pipe(fs.createWriteStream('images/out_2.png'));
 });
+
+console.log(base);
+
+fs.createReadStream('images/ps.png');
