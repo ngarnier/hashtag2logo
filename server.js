f029505b5b23d9bd8538952c5f9c46254deffa18
Nicolas Garnier <ngarnier@mailjet.com>
2015-10-24 03:07:44 +0200

    Added mosaic from twitter pics

--- server.js
+++ server.js
@@ -5,7 +5,10 @@ var express = require('express'),
 	app = express(),
 	credentials = require('./credentials.js'),
 	Twit = require('twit'),
-	T = new Twit(credentials.twitter);
+	T = new Twit(credentials.twitter),
+	fs = require('fs'),
+	gm = require('gm'),
+	imagesArray = [];
 
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
@@ -31,22 +34,39 @@ function handleAttachment(att){
 
 function handleHashtag(hash){
 	//Look for the hashtag on twitter
-	T.get('search/tweets', { q: 'aghacks', count: 20 }, function(err, data, response) {
+	T.get('search/tweets', { q: 'aghacks', count: 4 }, function(err, data, response) {
 		for (var i = 0; i < data.statuses.length; i++) {
 			if (i == 0) {
-				console.log(data.statuses[i].user.profile_image_url);
+				imagesArray.push(data.statuses[i].user.profile_image_url);
 			}
 			else if (data.statuses[i].user.profile_image_url !== data.statuses[i-1].user.profile_image_url) {
-				console.log("---------" + data.statuses[i].user.profile_image_url);
+				imagesArray.push(data.statuses[i].user.profile_image_url);
 			}
 		};
   		if (err) {
   			console.log(err);
   		}
-	})
+  			// Get all the pictures of all the users and put them in a single photo
+	// a b c d  ->  ab
+//              cd
+console.log("ok");
+gm()
+    .in('-page', '+0+0')  // Custom place for each of the images
+    .in(imagesArray[0])
+    .in('-page', '+48+0')
+    .in(imagesArray[1])
+    .in('-page', '+0+48')
+    .in(imagesArray[2])
+    .in('-page', '+48+48')
+    .in(imagesArray[3])
+    .minify()  // Halves the size, 512x512 -> 256x256
+    .mosaic()  // Merges the images as a matrix
+    .write('output.jpg', function (err) {
+        if (err) console.log(err);
+    });
+  		console.log(imagesArray);
+	});
 	// Get all users who tweeted on the #
-
-	// Get all the pictures of all the users and put them in a single photo
 
 	//Turn that photo into the logo (merge with handleAttachment)
 }
