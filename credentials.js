1269d009ef8120ef1c5a545e4eee7798b5468f99
Nicolas Garnier <ngarnier@users.noreply.github.com>
2015-10-24 13:46:27 +0200

    Delete credentials.js

--- credentials.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-   twitter:{
-       consumer_key: 'dvLjeiWsbBVw6WO8taz5zVd5p',
-       consumer_secret: 'yIMWy8VoofNFWEfJauq5YHOszdH2lIgmXf1mrOCZvSAUYmR2Nt',
-       access_token: '26494028-qos13Ge5cizIK2v5neABMifW5eE5ZZbFNYmBOkvlv',
-       access_token_secret: '5TBcNjcs6rEGRMdwyKfkAwh1e3q9eRe3fp3PxjVhXhrHs'
-   }
-}
