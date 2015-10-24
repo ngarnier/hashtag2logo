c892df023237bab9808438a0b10d41a98b5dccd7
SHARMA Shubham <shubham@sharma.fr>
2015-10-24 02:54:26 +0200

    image overlap ok

--- image_test2.js
+++ image_test2.js
@@ -1,29 +1,43 @@
 var fs = require('fs'),
     PNG = require('pngjs').PNG;
 
-var base = [];
+var base = fs.createReadStream('images/ps.png')
 
-fs.createReadStream('images/google.png')
 .pipe(new PNG({
     filterType: 4
 }))
+
 .on('parsed', function() {
 
-    for (var y = 0; y < this.height; y++) {
-        for (var x = 0; x < this.width; x++) {
-            var idx = (this.width * y + x) << 2;
+    var up = fs.createReadStream('images/google.png')
 
-            base[idx] = this.data[idx];
-            base[idx] = this.data[idx+1];
-            base[idx] = this.data[idx+2];
-            base[idx] = this.data[idx+3];
+    .pipe(new PNG({
+        filterType: 4
+    }))
 
+    .on('parsed', function() {
+
+        for (var y = 0; y < base.height; y++) {
+            for (var x = 0; x < base.width; x++) {
+                var idx = (base.width * y + x) << 2;
+
+                if (base.data[idx] == 0 && base.data[idx + 1] == 0 && base.data[idx + 2] == 0){
+                    base.data[idx] = 255;
+                    base.data[idx+1] = 255;
+                    base.data[idx+2] = 255;
+                    base.data[idx+3] = 255;
+                }
+                else{
+                    base.data[idx] = up.data[idx];
+                    base.data[idx+1] = up.data[idx + 1];
+                    base.data[idx+2] = up.data[idx + 2];
+                    base.data[idx+3] = up.data[idx + 3];
+                }
+            }
         }
-    }
 
-    this.pack().pipe(fs.createWriteStream('images/out_2.png'));
+        base.pack().pipe(fs.createWriteStream('images/out_2.png'));
+    });
+
 });
 
-console.log(base);
-
-fs.createReadStream('images/ps.png');
