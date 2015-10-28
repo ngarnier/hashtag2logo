fbfb84a9c9fa487f0da033839f5e006d310d0f52
Nicolas Garnier <ngarnier@mailjet.com>
2015-10-28 12:13:37 +0100

    BugFix on mosaic.js (called mosaic2)
    + typos & documenting on server.js

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.post('/parse/', function (req, res) {
 	var timestamp = Math.floor(new Date() / 1000),
 	new_attachement_name = hashtag + "_" + timestamp + ".png";
 
-	console.log("--> Mail recived from " + sender + "whith the hashtag #" + hashtag);
+	console.log("--> Mail recived from " + sender + "with the hashtag #" + hashtag);
 
 	handleHashtag(hashtag, function(){
 		require("fs").writeFile("attachment/" + new_attachement_name, attachmentb64, 'base64', function(err) {
@@ -67,21 +67,21 @@ function handleAttachment(att){
 function handleHashtag(hash, callback){
 	//Look for the hashtag on twitter
 	T.get('search/tweets', { q: hash, count: 100 }, function(err, data, response) {
-		//Store all the pics of all users who tweeted in imagesArray
+		//Store all the pics of all users who tweeted in imagesArray (first page/100)
 		if (err) {
   			console.log(err);
   			return;
   		}
 
 		T.get('search/tweets', { cursor:data.next_cursor, q: hash, count: 100 }, function(err, data2, response) {
-			//Store all the pics of all users who tweeted in imagesArray
+			//Store all the pics of all users who tweeted in imagesArray (second page/200)
 			if (err) {
 	  			console.log(err);
 	  			return;
 	  		}
 
 			T.get('search/tweets', { cursor:data2.next_cursor, q: hash, count: 100 }, function(err, data3, response) {
-				//Store all the pics of all users who tweeted in imagesArray
+				//Store all the pics of all users who tweeted in imagesArray (third page/300)
 				if (err) {
 		  			console.log(err);
 		  			return;
